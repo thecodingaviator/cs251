96e7df6e4609773c0532ea20504b924e18ff3261
Parth Parth <34807532+thecodingaviator@users.noreply.github.com>
2023-02-23 22:43:08 -0500

    task 2 p2

---
+++ project2/transformation.py
@@ -0,0 +1,333 @@
+'''transformation.py
+Perform projections, translations, rotations, and scaling operations on Numpy ndarray data.
+Parth Parth
+CS 251 Data Analysis Visualization
+Spring 2023
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+import palettable
+import analysis
+import data
+
+
+class Transformation(analysis.Analysis):
+
+    def __init__(self, orig_dataset, data=None):
+        '''Constructor for a Transformation object
+
+        Parameters:
+        -----------
+        orig_dataset: Data object. shape=(N, num_vars).
+            Contains the original dataset (only containing all the numeric variables,
+            `num_vars` in total).
+        data: Data object (or None). shape=(N, num_proj_vars).
+            Contains all the data samples as the original, but ONLY A SUBSET of the variables.
+            (`num_proj_vars` in total). `num_proj_vars` <= `num_vars`
+
+        TODO:
+        - Pass `data` to the superclass constructor.
+        - Create an instance variable for `orig_dataset`.
+        '''
+
+        super().__init__(data)
+
+        self.orig_dataset = orig_dataset
+
+    def project(self, headers):
+        '''Project the original dataset onto the list of data variables specified by `headers`,
+        i.e. select a subset of the variables from the original dataset.
+        In other words, your goal is to populate the instance variable `self.data`.
+
+        Parameters:
+        -----------
+        headers: Python list of str. len(headers) = `num_proj_vars`, usually 1-3 (inclusive), but
+            there could be more.
+            A list of headers (strings) specifying the feature to be projected onto each axis.
+            For example: if headers = ['hi', 'there', 'cs251'], then the data variables
+                'hi' becomes the 'x' variable,
+                'there' becomes the 'y' variable,
+                'cs251' becomes the 'z' variable.
+            The length of the list matches the number of dimensions onto which the dataset is
+            projected — having 'y' and 'z' variables is optional.
+
+        TODO:
+        - Create a new `Data` object that you assign to `self.data` (project data onto the `headers`
+        variables). Determine and fill in 'valid' values for all the `Data` constructor
+        keyword arguments (except you dont need `filepath` because it is not relevant here).
+        '''
+
+        h2c = {}
+        for i in range(len(headers)):
+            h2c[headers[i]] = i
+
+        self.data = data.Data(headers = headers, data = self.orig_dataset, header2col = h2c)
+        
+    def get_data_homogeneous(self):
+        '''Helper method to get a version of the projected data array with an added homogeneous
+        coordinate. Useful for homogeneous transformations.
+
+        Returns:
+        -----------
+        ndarray. shape=(N, num_proj_vars+1). The projected data array with an added 'fake variable'
+        column of ones on the right-hand side.
+            For example: If we have the data SAMPLE (just one row) in the projected data array:
+            [3.3, 5.0, 2.0], this sample would become [3.3, 5.0, 2.0, 1] in the returned array.
+
+        NOTE:
+        - Do NOT update self.data with the homogenous coordinate.
+        '''
+
+        data_orig = self.orig_dataset.get_all_data()
+
+        data_orig = np.hstack((data_orig, np.ones((data_orig.shape[0], 1))))
+
+        return data_orig
+
+    def translation_matrix(self, magnitudes):
+        ''' Make an M-dimensional homogeneous transformation matrix for translation,
+        where M is the number of features in the projected dataset.
+
+        Parameters:
+        -----------
+        magnitudes: Python list of float.
+            Translate corresponding variables in `headers` (in the projected dataset) by these
+            amounts.
+
+        Returns:
+        -----------
+        ndarray. shape=(num_proj_vars+1, num_proj_vars+1). The transformation matrix.
+
+        NOTE: This method just creates the translation matrix. It does NOT actually PERFORM the
+        translation!
+        '''
+        
+        mags = np.eye(len(magnitudes) + 1)
+
+        for i in range(len(magnitudes)):
+            mags[i][len(magnitudes)] = magnitudes[i]
+
+        return mags
+
+    def scale_matrix(self, magnitudes):
+        '''Make an M-dimensional homogeneous scaling matrix for scaling, where M is the number of
+        variables in the projected dataset.
+
+        Parameters:
+        -----------
+        magnitudes: Python list of float.
+            Scale corresponding variables in `headers` (in the projected dataset) by these amounts.
+
+        Returns:
+        -----------
+        ndarray. shape=(num_proj_vars+1, num_proj_vars+1). The scaling matrix.
+
+        NOTE: This method just creates the scaling matrix. It does NOT actually PERFORM the scaling!
+        '''
+        
+        scales = np.eye(len(magnitudes) + 1)
+
+        for i in range(len(magnitudes)):
+            scales[i][i] = magnitudes[i]
+
+        return scales
+
+    def translate(self, magnitudes):
+        '''Translates the variables `headers` in projected dataset in corresponding amounts specified
+        by `magnitudes`.
+
+        Parameters:
+        -----------
+        magnitudes: Python list of float.
+            Translate corresponding variables in `headers` (in the projected dataset) by these amounts.
+
+        Returns:
+        -----------
+        ndarray. shape=(N, num_proj_vars). The translated data (with all variables in the projected).
+            dataset. NOTE: There should be NO homogenous coordinate!
+
+        TODO:
+        - Use matrix multiplication to translate the projected dataset, as advertised above.
+        - Update `self.data` with a NEW Data object with the SAME `headers` and `header2col`
+        dictionary as the current `self.data`, but DIFFERENT data (set to the data you
+        transformed in this method). NOTE: The updated `self.data` SHOULD NOT have a homogenous
+        coordinate!
+        '''
+        
+        Dh = self.get_data_homogeneous()
+
+        T = self.translation_matrix(magnitudes)
+
+        Dh = T @ Dh.T
+
+        self.data = data.Data(headers = self.data.headers, data = Dh[:, :-1], header2col = self.data.header2col)
+
+        return Dh[:, :-1]
+
+    def scale(self, magnitudes):
+        '''Scales the variables `headers` in projected dataset in corresponding amounts specified
+        by `magnitudes`.
+
+        Parameters:
+        -----------
+        magnitudes: Python list of float.
+            Scale corresponding variables in `headers` (in the projected dataset) by these amounts.
+
+        Returns:
+        -----------
+        ndarray. shape=(N, num_proj_vars). The scaled data (with all variables in the projected).
+            dataset. NOTE: There should be NO homogenous coordinate!
+
+        TODO:
+        - Use matrix multiplication to scale the projected dataset, as advertised above.
+        - Update `self.data` with a NEW Data object with the SAME `headers` and `header2col`
+        dictionary as the current `self.data`, but DIFFERENT data (set to the data you
+        transformed in this method). NOTE: The updated `self.data` SHOULD NOT have a
+        homogenous coordinate!
+        '''
+        
+        Dh = self.get_data_homogeneous()
+        S = self.scale_matrix(magnitudes)
+
+        Dh = np.matmul(Dh, S)
+
+        self.data = data.Data(headers = self.data.headers, data = Dh[:, :-1], header2col = self.data.header2col)
+
+        return Dh[:, :-1]
+
+    def transform(self, C):
+        '''Transforms the PROJECTED dataset by applying the homogeneous transformation matrix `C`.
+
+        Parameters:
+        -----------
+        C: ndarray. shape=(num_proj_vars+1, num_proj_vars+1).
+            A homogeneous transformation matrix.
+
+        Returns:
+        -----------
+        ndarray. shape=(N, num_proj_vars). The projected dataset after it has been transformed by `C`
+
+        TODO:
+        - Use matrix multiplication to apply the compound transformation matix `C` to the projected
+        dataset.
+        - Update `self.data` with a NEW Data object with the SAME `headers` and `header2col`
+        dictionary as the current `self.data`, but DIFFERENT data (set to the data you
+        transformed in this method). NOTE: The updated `self.data` SHOULD NOT have a homogenous
+        coordinate!
+        '''
+        
+        Dh = self.get_data_homogeneous()
+
+        Dh = np.matmul(Dh, C)
+
+        self.data = data.Data(headers = self.data.headers, data = Dh[:, :-1], header2col = self.data.header2col)
+
+        return Dh[:, :-1]
+
+    def normalize_together(self):
+        '''Normalize all variables in the projected dataset together by translating the global minimum
+        (across all variables) to zero and scaling the global range (across all variables) to one.
+
+        You should normalize (update) the data stored in `self.data`.
+
+        Returns:
+        -----------
+        ndarray. shape=(N, num_proj_vars). The normalized version of the projected dataset.
+
+        NOTE: Given the goal of this project, for full credit you should implement the normalization
+        using matrix multiplications (matrix transformations).
+        '''
+        
+        pass
+
+    def normalize_separately(self):
+        '''Normalize each variable separately by translating its local minimum to zero and scaling
+        its local range to one.
+
+        You should normalize (update) the data stored in `self.data`.
+
+        Returns:
+        -----------
+        ndarray. shape=(N, num_proj_vars). The normalized version of the projected dataset.
+
+        NOTE: Given the goal of this project, for full credit you should implement the normalization
+        using matrix multiplications (matrix transformations).
+        '''
+        pass
+
+    def rotation_matrix_3d(self, header, degrees):
+        '''Make an 3-D homogeneous rotation matrix for rotating the projected data
+        about the ONE axis/variable `header`.
+
+        Parameters:
+        -----------
+        header: str. Specifies the variable about which the projected dataset should be rotated.
+        degrees: float. Angle (in degrees) by which the projected dataset should be rotated.
+
+        Returns:
+        -----------
+        ndarray. shape=(4, 4). The 3D rotation matrix with homogenous coordinate.
+
+        NOTE: This method just creates the rotation matrix. It does NOT actually PERFORM the rotation!
+        '''
+        pass
+
+    def rotate_3d(self, header, degrees):
+        '''Rotates the projected data about the variable `header` by the angle (in degrees)
+        `degrees`.
+
+        Parameters:
+        -----------
+        header: str. Specifies the variable about which the projected dataset should be rotated.
+        degrees: float. Angle (in degrees) by which the projected dataset should be rotated.
+
+        Returns:
+        -----------
+        ndarray. shape=(N, num_proj_vars). The rotated data (with all variables in the projected).
+            dataset. NOTE: There should be NO homogenous coordinate!
+
+        TODO:
+        - Use matrix multiplication to rotate the projected dataset, as advertised above.
+        - Update `self.data` with a NEW Data object with the SAME `headers` and `header2col`
+        dictionary as the current `self.data`, but DIFFERENT data (set to the data you
+        transformed in this method). NOTE: The updated `self.data` SHOULD NOT have a
+        homogenous coordinate!
+        '''
+        pass
+
+    def scatter_color(self, ind_var, dep_var, c_var, title=None):
+        '''Creates a 2D scatter plot with a color scale representing the 3rd dimension.
+
+        Parameters:
+        -----------
+        ind_var: str. Header of the variable that will be plotted along the X axis.
+        dep_var: Header of the variable that will be plotted along the Y axis.
+        c_var: Header of the variable that will be plotted along the color axis.
+            NOTE: Use a ColorBrewer color palette (e.g. from the `palettable` library).
+        title: str or None. Optional title that will appear at the top of the figure.
+        '''
+        
+        # Set up colors
+        color_map = palettable.colorbrewer.sequential.PuBu_9
+
+        # Make a scatter plot of dep_var vs. ind_var, with the color of each point
+        # representing the value of c_var.
+        plt.scatter(self.orig_dataset.data[:, self.orig_dataset.header2col[ind_var]],
+                    self.orig_dataset.data[:, self.orig_dataset.header2col[dep_var]],
+                    c=self.orig_dataset.data[:, self.orig_dataset.header2col[c_var]],
+                    cmap=color_map.mpl_colormap)
+        
+        # Add a color bar to the right of the plot.
+        plt.colorbar().set_label(c_var)
+
+        # Add labels to the axes.
+        plt.xlabel(ind_var)
+        plt.ylabel(dep_var)
+
+        # Add a title if one was provided.
+        if title is not None:
+            plt.title(title)
+
+        # Show the plot.
+        plt.show()
+        
